adeb51706213a7f25ed6a8832d30ff0691b2d37f
Marek Schir <schir2@msn.com>
2021-07-29 22:59:08 -0400

    added profile to admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
