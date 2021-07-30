25aa8b97f7bee4a6d6163d163dae44ac240c7630
Marek Schir <schir2@msn.com>
2021-07-30 00:27:35 -0400

    fixed get resize to avatar method

--- users/ustils.py
+++ users/ustils.py
@@ -1,4 +1,7 @@
 from io import BytesIO
+from PIL import Image
+from django.core import files
+from django.contrib.auth import get_user_model
 
 import requests
 
@@ -14,3 +17,21 @@ def download_image(url: str):
     fp = BytesIO()
     fp.write(response.content)
     return fp
+
+
+def resize_to_avatar(infile: BytesIO) -> BytesIO:
+    outfile = BytesIO()
+    image = Image.open(infile, 'r')
+    image = image.resize((64,64), Image.ANTIALIAS)
+    image.save(outfile, format='JPEG', quality=100)
+    outfile.seek(0)
+    return outfile
+
+
+if __name__ == '__main__':
+    fp = download_image('https://pocket-syndicated-images.s3.amazonaws.com/5ddedbe927fc7.jpg')
+    resized = resize_to_avatar(fp)
+    f = files.File(resized)
+    Users = get_user_model()
+    p = Users.objects.all()[0].profile
+    p.profile_avatar.save('test.jpg', resized)
