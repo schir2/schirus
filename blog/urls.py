f58a541991ba0a6e12c63c99b8f281d419c5cf12
Marek Schir <schir2@msn.com>
2021-08-23 20:46:57 -0400

    swapped the order of post detail url from pk/slug to slug/pk

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('', PostListView.as_view(), name='home'),
     path('create', PostCreateView.as_view(), name='post-create'),
-    path('<int:pk>-<str:slug>/', include([
+    path('<str:slug>-<int:pk>/', include([
         path('', PostDetailView.as_view(), name='post-detail'),
         path('update', PostUpdateView.as_view(), name='post-update'),
         path('delete', PostDeleteView.as_view(), name='post-delete'),
