dbb91891a0ce69ca4effac95557e85f96a84e763
Marek Schir <schir2@msn.com>
2021-08-23 20:45:27 -0400

    added modal

---
+++ blog/static/blog/js/modal.js
@@ -0,0 +1,10 @@
+let modal = document.getElementsByClassName('modal')[0]
+
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
