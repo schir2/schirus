964afbe233f84e96663d1b41bd1698b2c7b2f416
Marek Schir <schir2@msn.com>
2021-12-06 22:01:57 -0500

    added like action

--- blog/viewsets.py
+++ blog/viewsets.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from blog.serializers import CategorySerializer, LikeSerializer, PostSerializer, UserSerializer
 from blog.models import Category, Like, Post
 from django.contrib.auth import get_user_model
@@ -14,6 +17,13 @@ class UserViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+    @action(detail=True)
+    def like(self, request, *args, **kwargs):
+        post = self.get_object()
+        print(post.like)
+        print(request.user)
+        return Response(self.serializer_class(instance=post, context={'request':request}).data, )
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
