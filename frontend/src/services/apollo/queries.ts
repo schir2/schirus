c8ed215089dada3198205745c11be0819c711b70
Marek Schir <schir2@msn.com>
2022-02-17 22:49:50 -0500

    added get article by index query

--- frontend/src/services/apollo/queries.ts
+++ frontend/src/services/apollo/queries.ts
@@ -26,3 +26,29 @@ export const GET_ARTICLE_LIST = gql`
         }
     }
 `
+export const GET_ARTICLE_BY_ID = gql`
+    query ($id: String!){
+        article(id: $id){
+            id,
+            slug,
+            title,
+            content,
+            categories {
+                id,
+                slug,
+                name
+            },
+            createdOn,
+            updatedOn,
+            likes {
+                user {id, username}
+                createdOn
+            }
+            user {
+                username,
+                firstName,
+                lastName
+            }
+        }
+    }
+`
