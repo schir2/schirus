cfb1400b0d85154c35e0d6ffc4527898fdff15af
Marek Schir <schir2@msn.com>
2022-02-17 22:49:15 -0500

    added vlink, vbutton, vcardsmall and vbadge components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,27 +1,31 @@
-import App from "@/App.vue";
-import {createApp} from "vue";
+import App from "@/App.vue"
+import {createApp} from "vue"
 import 'reflect-metadata'
 import router from '@/router'
-import store from "@/store";
-import VueToast from 'vue-toast-notification';
+import store from "@/store"
+import VueToast from 'vue-toast-notification'
 
 import {createApolloProvider} from '@vue/apollo-option'
 import {ApolloClient, InMemoryCache} from '@apollo/client/core'
-const typeDefs = require("@/services/apollo/schema.ts")
+import {QuillEditor} from "@vueup/vue-quill"
+import AuthSmall from "@/components/shared/AuthSmall.vue"
+import NavLink from "@/components/shared/NavLink.vue"
+import TheHeader from "@/components/shared/TheHeader.vue"
+import PrimaryNav from "@/components/shared/PrimaryNav.vue"
+import VLink from "@/components/shared/VLink.vue"
+import VCardSmall from "@/components/shared/VCardSmall.vue"
+import VButton from "@/components/shared/VButton.vue"
+import VBadge from "@/components/shared/VBadge.vue"
 
-import {QuillEditor} from "@vueup/vue-quill";
-import AuthSmall from "@/components/shared/AuthSmall.vue";
-import NavLink from "@/components/shared/NavLink.vue";
-import TheHeader from "@/components/shared/TheHeader.vue";
-import PrimaryNav from "@/components/shared/PrimaryNav.vue";
-import VCard from "@/components/shared/VCard.vue"
 // CSS Imports
 import "normalize.css"
 import "animate.css"
 import '@/scss/style.scss'
-import 'vue-toast-notification/dist/theme-sugar.css';
-import 'material-icons/iconfont/material-icons.css';
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import 'vue-toast-notification/dist/theme-sugar.css'
+import 'material-icons/iconfont/material-icons.css'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+const typeDefs = require("@/services/apollo/schema.ts")
 
 
 const cache = new InMemoryCache()
@@ -43,7 +47,10 @@ app.component('AuthSmall', AuthSmall)
 app.component('NavLink', NavLink)
 app.component('TheHeader', TheHeader)
 app.component('PrimaryNav', PrimaryNav)
-app.component('VCard', VCard)
+app.component('VCardSmall', VCardSmall)
+app.component('VLink', VLink)
+app.component('VButton', VButton)
+app.component('VBadge', VBadge)
 
 app.use(router)
 app.use(store)
