a4f952e6e43749ff714cd22f8b18f5bb1d9be6a0
Marek Schir <schir2@msn.com>
2022-02-17 22:47:07 -0500

    added apollo request and basic rendering

--- frontend/src/pages/blog/ArticleDetailPage.vue
+++ frontend/src/pages/blog/ArticleDetailPage.vue
@@ -1,10 +1,26 @@
 <template>
-<div></div>
+  <div v-if="article">
+    <TheHeader><h1>{{ article.title }}</h1></TheHeader>
+    <h2 v-if="article.user">Written by {{ article.user.username }}</h2>
+    <article v-if="article.content" v-html="article.content">
+    </article>
+
+  </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
   name: "ArticleDetailPage",
+  computed: {
+    ...mapState({
+      article: state => state.blog.article
+    })
+  },
+  mounted() {
+    this.$store.dispatch("blog/getArticle", this.$route.params.id)
+  }
 }
 </script>
 
