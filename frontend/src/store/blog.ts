ed42226f0675332be65ba87b3309f128d9910e81
Marek Schir <schir2@msn.com>
2022-02-17 22:47:44 -0500

    renamed some actions and mutations and added get article action and mutation

--- frontend/src/store/blog.ts
+++ frontend/src/store/blog.ts
@@ -2,7 +2,7 @@ import {ActionTree, GetterTree, MutationTree} from "vuex";
 import Article from "@/models/Article";
 import User from "@/models/User";
 import {apolloClient} from "@/main";
-import {GET_ARTICLE_LIST} from "@/services/apollo/queries";
+import {GET_ARTICLE_BY_ID, GET_ARTICLE_LIST} from "@/services/apollo/queries";
 
 export class BlogState {
     article: Article = new Article()
@@ -11,19 +11,32 @@ export class BlogState {
 
 const actions = <ActionTree<BlogState, any>>{
 
-    async getListTop({commit}) {
+    async getArticlesTop({commit}) {
         return apolloClient.query(
             {
                 query: GET_ARTICLE_LIST
             }
         ).then(response => {
             if (response?.data?.articles)
-            commit("getList", response.data.articles)
+                commit("setArticles", response.data.articles)
         })
 
     },
-    async getListByUser(state, user: User) {
+    async getArticlesListByUser(state, user: User) {
     },
+
+    async getArticle({commit}, id) {
+        return apolloClient.query(
+            {
+                query: GET_ARTICLE_BY_ID,
+                variables: {id: id}
+            }
+        ).then(response => {
+            if (response?.data?.article) {
+                commit("setArticle", response.data.article)
+            }
+        })
+    }
 
 }
 const getters = <GetterTree<BlogState, any>>{
@@ -32,8 +45,11 @@ const getters = <GetterTree<BlogState, any>>{
     }
 }
 const mutations = <MutationTree<BlogState>>{
-    getList(state, articles: Article[]){
+    setArticles(state, articles: Article[]) {
         state.articles = articles
+    },
+    setArticle(state, article: Article) {
+        state.article = article
     }
 }
 const blogStore = {
