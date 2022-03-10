ffdc4a492023f4f9f8999078440a545991d4673e
Marek Schir <schir2@gmail.com>
2022-03-10 00:55:00 -0500

    added ability to upload directories

--- frontend/src/pages/apps/PDFMerge.vue
+++ frontend/src/pages/apps/PDFMerge.vue
@@ -2,38 +2,38 @@
   <div @dragover.prevent @drop.prevent>
     <form>
       <draggable-component
-        v-model="files"
-        class="files"
-        item-key="id"
-        @start="drag = true"
-        @end="drag = false"
+          v-model="files"
+          class="files"
+          item-key="id"
+          @start="drag = true"
+          @end="drag = false"
       >
         <template #item="{ element, index }">
           <div class="file">
             <button
-              class="material-icons file-action"
-              @click.prevent="removeFile(index)"
+                class="material-icons file-action"
+                @click.prevent="removeFile(index)"
             >
               delete
             </button>
             <span class="file-heading">{{
-              element.file.name.substring(0, element.file.name.length - 4)
-            }}</span>
-            <iframe class="file-thumbnail" :src="element.thumbnail" />
+                element.file.name.substring(0, element.file.name.length - 4)
+              }}</span>
+            <iframe class="file-thumbnail" :src="element.thumbnail"/>
           </div>
         </template>
       </draggable-component>
       <input
-        ref="fileInput"
-        type="file"
-        multiple
-        style="display: none"
-        @change="onUploadHandler"
+          ref="fileInput"
+          type="file"
+          multiple
+          style="display: none"
+          @change="onUploadHandler"
       />
       <div
-        class="drop-zone"
-        @drop="onDropHandler"
-        @click="$refs.fileInput.click()"
+          class="drop-zone"
+          @drop="onDropHandler"
+          @click="$refs.fileInput.click()"
       >
         <span class="material-icons icon">file_upload</span>
         <span class="display-1">Drop Your Files Here</span>
@@ -44,58 +44,66 @@
 </template>
 
 <script>
-import { pdfService } from "@/services/PDFService";
-import draggableComponent from "vuedraggable";
+import {pdfService} from "@/services/PDFService"
+import draggableComponent from "vuedraggable"
+import {FileDirectory, traverseDirectories} from "@/pages/apps/types";
 
 export default {
   name: "PDFMerge",
-  components: { draggableComponent },
+  components: {draggableComponent},
   data() {
     return {
       files: [],
+      directory: null,
       drag: false,
-    };
+    }
   },
   methods: {
     async onClickMerge() {
       const objectUrl = await pdfService.generateMergedObjectUrl(
-        this.files.map((file) => file.file)
-      );
-      window.open(objectUrl, "", "height=650,width=840");
+          this.files.map((file) => file.file)
+      )
+      window.open(objectUrl, "", "height=650,width=840")
     },
     async addFiles(files) {
-      const success = [];
-      const errors = [];
+      const success = []
+      const errors = []
       for (const file of files) {
         if (file.name.endsWith(".pdf")) {
           this.files.push({
             file: file,
             thumbnail: await pdfService.generateThumbnailObjectUrl(file),
-          });
-          success.push(file.name);
+          })
+          success.push(file.name)
         } else {
-          errors.push(file.name);
+          errors.push(file.name)
         }
       }
       if (success.length !== 0) {
-        this.$toast.success(`Added ${success.length} files.`);
+        this.$toast.success(`Added ${success.length} files.`)
       }
       if (errors.length !== 0) {
-        this.$toast.error(`Failed to add ${errors.length} files.`);
+        this.$toast.error(`Failed to add ${errors.length} files.`)
       }
     },
     removeFile(index) {
-      this.$toast.warning(`Removed ${this.files[index].file.name}`);
-      this.files.splice(index, 1);
+      this.$toast.warning(`Removed ${this.files[index].file.name}`)
+      this.files.splice(index, 1)
     },
     async onUploadHandler(event) {
-      this.addFiles(event.target.files);
+      await this.addFiles(event.target.files)
     },
     async onDropHandler(event) {
-      this.addFiles(event.dataTransfer.files);
+      await this.addFiles(event.dataTransfer.files)
+      const entries = []
+      for (const item of event.dataTransfer.items){
+        entries.push(item.webkitGetAsEntry())
+      }
+      this.directory = traverseDirectories(entries)
+      console.log(this.directory)
     },
   },
-};
+}
 </script>
 
 <style scoped lang="scss">
